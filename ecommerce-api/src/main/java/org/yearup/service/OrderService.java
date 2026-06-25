565d81b506dd6ed1b94f99164e06a817d4389a42
Michael Okeke <michaelokeke123@gmail.com>
2026-06-24 22:48:31 -0500

    Added: Added Order service

---
+++ ecommerce-api/src/main/java/org/yearup/service/OrderService.java
@@ -0,0 +1,73 @@
+package org.yearup.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.yearup.models.*;
+import org.yearup.repository.OrderLineItemRepository;
+import org.yearup.repository.OrderRepository;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Service
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+    private final OrderLineItemRepository orderLineItemRepository;
+    private final ShoppingCartService shoppingCartService;
+    private final ProfileService profileService;
+
+    public OrderService(OrderRepository orderRepository,
+                        OrderLineItemRepository orderLineItemRepository,
+                        ShoppingCartService shoppingCartService,
+                        ProfileService profileService) {
+
+        this.orderRepository = orderRepository;
+        this.orderLineItemRepository = orderLineItemRepository;
+        this.shoppingCartService = shoppingCartService;
+        this.profileService = profileService;
+    }
+
+    @Transactional
+    public Order checkout(int userId) {
+
+        ShoppingCart cart = shoppingCartService.getByUserId(userId);
+
+        Profile profile = profileService.getByUserId(userId)
+                .orElseThrow();
+
+        Order order = new Order();
+        order.setUserId(userId);
+        order.setDate(LocalDateTime.now());
+
+        order.setAddress(profile.getAddress());
+        order.setCity(profile.getCity());
+        order.setState(profile.getState());
+        order.setZip(profile.getZip());
+
+        order.setShippingAmount(BigDecimal.ZERO);
+
+        order = orderRepository.save(order);
+
+        for (ShoppingCartItem item : cart.getItems().values()) {
+
+            OrderLineItem lineItem = new OrderLineItem();
+
+            lineItem.setOrderId(order.getOrderId());
+            lineItem.setProductId(item.getProduct().getProductId());
+            lineItem.setQuantity(item.getQuantity());
+
+            lineItem.setSalesPrice(
+                    BigDecimal.valueOf(item.getProduct().getPrice()));
+
+            lineItem.setDiscount(
+                    BigDecimal.valueOf(item.getDiscountPercent()));
+
+            orderLineItemRepository.save(lineItem);
+        }
+
+        shoppingCartService.clearCart(userId);
+
+        return order;
+    }
+}
